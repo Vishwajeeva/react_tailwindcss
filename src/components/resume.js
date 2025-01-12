f6f0cdb79d87dc56c598a1c6f0c96f8bdf57ccb9
Vishwa j <108922362+Vishwajeeva@users.noreply.github.com>
2025-01-12 16:06:09 +0530

    Update resume.js

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,7 +2,7 @@ import AboutImg from '../assets/resume1.jpeg';
 
 export default function resume() {
     const config ={
-        link:'https://drive.google.com/file/d/1fSLcwfZ3my90-kyJqKaLGJaSZSUb93xa/view?usp=sharing'
+        link:'https://docs.google.com/document/d/1sksW8YlH3uRweaIvvEHDrUtdVxuPzOMe/edit?usp=sharing&ouid=117456025565975448553&rtpof=true&sd=true'
     }
     return (
         <section className="flex flex-col md:flex-row bg-secondary px-8 py-16 md:py-24 justify-center items-center "id='resume'>
